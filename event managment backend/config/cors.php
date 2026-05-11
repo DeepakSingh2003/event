86e530db73890389a85999866a824b07bdb81584
Deepka503200 <deepaksinghchauhan914@gmail.com>
2026-05-11 15:39:45 +0530

    fix fetch

--- event managment backend/config/cors.php
+++ event managment backend/config/cors.php
@@ -29,10 +29,12 @@ return [
 
     'allowed_origins' => array_filter(array_map('trim', explode(',', env(
         'CORS_ALLOWED_ORIGINS',
-        'http://localhost:5173,http://127.0.0.1:5173,http://localhost:8000,http://127.0.0.1:8000'
+        'http://localhost:5173,http://127.0.0.1:5173,http://localhost:8000,http://127.0.0.1:8000,https://event-production-b669.up.railway.app,https://spectacular-education-production-620e.up.railway.app'
     )))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/[a-z0-9-]+\.up\.railway\.app$/',
+    ],
 
     'allowed_headers' => ['*'],
 
